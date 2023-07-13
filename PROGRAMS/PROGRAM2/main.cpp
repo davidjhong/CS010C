a3bfcfc38ac68672bfe1fa664e856b5a5f36378b
David <112353499+davidjhong@users.noreply.github.com>
2023-07-13 01:04:50 -0700

    Update main.cpp
    test harness

--- PROGRAMS/PROGRAM2/main.cpp
+++ PROGRAMS/PROGRAM2/main.cpp
@@ -1,92 +1,84 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
+#include <limits>
+#include "BSTree.h"
+
 using namespace std;
 
-struct Node {
-    string payload;
-    Node* next;
-};
-
-Node* newNode(string payload) {
-   // Creates a newNode with user's payload input, sets the next to nullptr and returns the newNode 
-   Node * newNode = new Node;
-   newNode->payload = payload;
-   newNode->next = nullptr;
-   return newNode;
+void printOrders(BSTree *tree) {
+  cout << "Preorder = ";
+  tree->preOrder( );
+  cout << endl;
+  cout << "Inorder = ";
+  tree->inOrder( );
+  cout << endl;
+  cout << "Postorder = ";
+  tree->postOrder( );
+  cout << endl;
 }
 
-Node* loadGame(int n, vector<string> names) {
-    Node* head = nullptr;
-    Node* prev = nullptr;
-    string name;
+int menu() {
+  int choice = 0;
+  cout << endl << "Enter menu choice: ";
+  cout << endl;
+  cout 
+    << "1. Insert" << endl
+    << "2. Remove" << endl
+    << "3. Print" << endl
+    << "4. Search" << endl
+    << "5. Smallest" << endl
+    << "6. Largest" << endl
+    << "7. Height" << endl
+    << "8. Quit" << endl;
+  cin >> choice;
+  
+  // fix buffer just in case non-numeric choice entered
+  // also gets rid of newline character
+  cin.clear();
+   cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  return choice;
+}
 
-    for (int i = 0; i < n; ++i) {
-        name = names.at(i);
-        if (head == nullptr) {
-            head = newNode(name); // initialize head specially
-            prev = head; // Previous points to newNode aka head as well since list is empty
-        } else {
-            prev->next = newNode(name); // list is not empty, make the newNode next to head which is previous as well
-            prev = prev->next; // Previous points to the next
+int main( ) {
+    // Pass all your tests before running the executable main
+
+    BSTree tree;
+
+    int choice = menu();
+
+    string entry;
+  
+    while (choice != 8) {
+    
+        if (choice == 1) {
+          cout << "Enter string to insert: " << endl;
+          getline(cin, entry);
+          tree.insert(entry);
+        } else if (choice == 2) {
+          cout << "Enter string to remove: " << endl;
+          getline(cin, entry);
+          tree.remove(entry);
+        } else if (choice == 3) {
+          printOrders(&tree);
+        } else if (choice == 4) {
+          cout << "Enter string to search for: " << endl;
+          cin >> entry;
+          if(!tree.search(entry)){
+            cout << "Not Found";
+          } else {
+            cout << "Found";
+          }
+          cout << endl;
+        } else if (choice == 5) {
+          cout << "Smallest: " << tree.smallest() << endl;
+        } else if (choice == 6) {
+          cout << "Largest: "<< tree.largest() << endl;
+        } else if (choice == 7) {
+          cout << "Enter string: " << endl;
+          getline(cin, entry);
+          cout << "Height of subtree rooted at " << entry << ": "<< tree.height(entry) << endl;
         }
+        //fix buffer just in case non-numeric choice entered
+        choice = menu();
     }
-
-    if (prev != nullptr) {
-        prev->next = head; // make circular
-    }
-    return head;
-}
-
-void print(Node* start) { // prints list
-    Node* curr = start;
-    while (curr != nullptr) {
-        cout << curr->payload << endl;
-        curr = curr->next;
-        if (curr == start) {
-            break; // exit circular list
-        }
-    }
-}
-
-Node* runGame(Node* start, int k) { // josephus w circular list, k = num skips
-    Node* curr = start;
-    Node* prev = curr;
-    while (curr->next != curr) { // exit condition, last person standing
-        for (int i = 0; i < k; ++i) { // find kth node
-            prev = curr; // keep track of previous node
-            curr = curr->next; // moving nodes to the right
-        }
-
-        prev->next = curr->next; // delete kth node ; prev/curr => curr prev => 0 prev => 0 prev curr
-        delete curr;
-        curr = prev->next;
-    }
-
-    return curr; // last person standing
-}
-
-/* Driver program to test above functions */
-int main() {
-    int n=1, k=1, max; // n = num names; k = num skips (minus 1)
-    string name;
-    vector<string> names;
-
-    // get inputs
-    cin >> n >> k;
-    while (cin >> name && name != ".") { names.push_back(name); } // EOF or . ends input
-
-    // initialize and run game
-    Node* startPerson = loadGame(n, names);
-    Node* lastPerson = runGame(startPerson, k);
-
-    if (lastPerson != nullptr) {
-        cout << lastPerson->payload << " wins!" << endl;
-    } else {
-        cout << "error: null game" << endl;
-    }
-
     return 0;
 }
-
